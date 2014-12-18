299322661474318009688e722485c07cfbbbf9d1
Richard <legalize@xmission.com>
2014-12-18 11:20:30 -0700

    Handle basic driver based plotting in X11 driver

--- unix/os_unix.cpp
+++ unix/os_unix.cpp
@@ -1,3 +1,5 @@
+#include <cassert>
+
 #include <string.h>
 #include <sys/statvfs.h>
 
@@ -6,6 +8,7 @@
 #include "fractint.h"
 #include "mpmath.h"
 #include "prototyp.h"
+#include "drivers.h"
 
 // Global variables (yuck!)
 struct MP Ans = { 0 };
@@ -91,4 +94,59 @@ unsigned long get_disk_space()
 void init_failure(const char *message)
 {
     fputs(message, stderr);
+}
+
+extern void (*dotwrite)(int, int, int); // write-a-dot routine
+extern int (*dotread)(int, int);    // read-a-dot routine
+extern void (*linewrite)(int y, int x, int lastx, BYTE *pixels);     // write-a-line routine
+extern void (*lineread)(int y, int x, int lastx, BYTE *pixels);      // read-a-line routine
+
+#if defined(USE_DRIVER_FUNCTIONS)
+void set_normal_dot()
+{
+    dotwrite = driver_write_pixel;
+    dotread = driver_read_pixel;
+}
+#else
+static void driver_dot_write(int x, int y, int color)
+{
+    driver_write_pixel(x, y, color);
+}
+
+static int driver_dot_read(int x, int y)
+{
+    return driver_read_pixel(x, y);
+}
+
+void set_normal_dot()
+{
+    dotwrite = driver_dot_write;
+    dotread = driver_dot_read;
+}
+#endif
+
+void normaline(int y, int x, int lastx, BYTE *pixels)
+{
+    int width = lastx - x + 1;
+    assert(dotwrite);
+    for (int i = 0; i < width; i++)
+    {
+        (*dotwrite)(x + i, y, pixels[i]);
+    }
+}
+
+void normalineread(int y, int x, int lastx, BYTE *pixels)
+{
+    int width = lastx - x + 1;
+    assert(dotread);
+    for (int i = 0; i < width; i++)
+    {
+        pixels[i] = (*dotread)(x + i, y);
+    }
+}
+
+void set_normal_line()
+{
+    lineread = normalineread;
+    linewrite = normaline;
 }
